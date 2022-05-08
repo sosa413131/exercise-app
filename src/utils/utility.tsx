bc91d7e6c827e640d2ef7b913de1e050e42b3b23
David W Sosa <sosa.david88@gmail.com>
2022-05-08 00:40:23 -0400

    added firebase hosting and util directory

---
+++ src/utils/utility.tsx
@@ -0,0 +1,30 @@
+
+import { Geolocation } from '@capacitor/geolocation';
+
+interface coordinates {
+    lat: number;
+    long: number;
+    altitude: number;
+}
+
+
+var getCoordinates = async () => {
+    
+    try {
+        const geo = await Geolocation.getCurrentPosition();
+
+        var geoData: coordinates = {
+            lat: geo.coords.latitude ? geo.coords.latitude : NaN,
+            long: geo.coords.longitude ? geo.coords.longitude : NaN,
+            altitude: geo.coords.altitude ? geo.coords.altitude : NaN,
+        }
+        return geoData;
+
+    } catch (error: any) {
+
+        console.log(error);
+    }
+};
+
+export { getCoordinates };
+export type { coordinates };
